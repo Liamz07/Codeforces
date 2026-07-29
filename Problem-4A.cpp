1e2267e3d67ee57d4352fd19be770dea15530504
Minh Nguyen <nqnm7002@gmail.com>
2026-07-29 20:45:22 +0700

    Implement even number check in main function
    Handle edge case for number 2 and check for even numbers.

--- Problem-4A.cpp
+++ Problem-4A.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    int n; cin >> n;
+    if (n <= 2) cout << "NO\n"; //Trường hợp số 2 là edge case
+    else if (n % 2 == 0) cout << "YES\n"; //Với mọi số chẵn > 2 thì ta luôn có cách để chia thành 2 số chẵn
+    else cout << "NO\n";
+    return 0;
+}
